5bf54b97c29715dead07fb444b2569db98b9c957
Minhajul Abedin Ratul <minhajulabedinratul82@gmail.com>
2024-08-07 01:15:33 +0600

    Finished the project

--- Script.js
+++ Script.js
@@ -1,87 +1,157 @@
 
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 100);
+
     if (num >= 1 && num <= 33) {
-      return "ROCK";
+      cpuChoice.src = "./icon/angry.png";
+      return "BOSS";
     }
 
     else if (num >= 34 && num <= 66) {
-      return "PAPER";
+      cpuChoice.src = "./icon/espresso.png";
+      return "COFFEE MACHINE";
     }
 
-    else
-    return "SCISSOR";
-    
+    else {
+      cpuChoice.src = "./icon/internship.png";
+      return "INTERN";
+    }
   }
 
-  function getHumanChoice() {
-    let choice = prompt("Enter Rock or Paper or Scissor" );
-    return choice.toUpperCase();
-  }
+  const yourChoice = document.querySelector('#you');
+  const cpuChoice = document.querySelector('#cpu');
+
+function getHumanChoice(e) {
+    const target = e.target;
+
+    if (target.classList.contains('BOSS')) {
+        yourChoice.src = "./icon/angry.png";
+        return 'BOSS';
+    } else if (target.classList.contains('COFFEE-MACHINE')) {
+        yourChoice.src = "./icon/espresso.png";
+        return 'COFFEE MACHINE';
+    } else if (target.classList.contains('INTERN')) {
+        yourChoice.src = "./icon/internship.png";
+        return 'INTERN';
+    } else {
+        return null;
+    }
+}
+
+  const topText = document.querySelector('.text');
+  const para = document.querySelector('.first-text');
+  topText.appendChild(para);
+  const para1 = document.querySelector('.second-text');
+  topText.appendChild(para1);
+  const buttons = document.querySelector(".buttons");
+  let humanScore = 0, 
+  computerScore = 0;
 
   function playRound(humanChoice, computerChoice) {
 
     if (humanChoice === computerChoice) {
-      console.log("It's a draw");
-      return ( humanScore++ ,
-        computerScore++
-      );
+      para.textContent = "It's a draw";
+      para1.textContent = "The office remains a battleground of petty rivalries.";
     }
 
-    else if (humanChoice === "ROCK" && computerChoice === "SCISSOR") {
-      console.log("You won! , rock beats scissor");
-      return humanScore++;
-    }
-    
-    else if (humanChoice === "PAPER" && computerChoice === "ROCK") {
-      console.log("You won! , paper beats rock");
-      return humanScore++;
-    }
-
-    else if (humanChoice === "SCISSOR" && computerChoice === "PAPER") {
-      console.log("You won! , scissor beats rock");
-      return humanScore++;
-    }
-
-    else if (computerChoice === "ROCK" && humanChoice === "SCISSOR") {
-      console.log("You lost! , rock beats scissor")
-      return computerScore++;
+    else if (humanChoice === "BOSS" && computerChoice === "INTERN") {
+      para.textContent = "You won!";
+      para1.textContent = "Boss reprimands Intern";
+      humanScore++;
     }
     
-    else if (computerChoice === "PAPER" && humanChoice === "ROCK") {
-      console.log("You lost! , paper beats rock")
-      return computerScore++;
+    else if (humanChoice === "INTERN" && computerChoice === "COFFEE MACHINE") {
+      para.textContent = "You won!";
+      para1.textContent = "Intern breaks Coffee Machine";
+      humanScore++;
     }
 
-    else if (computerChoice === "SCISSOR" && humanChoice === "PAPER") {
-      console.log("You lost! , scissor beats paper")
-      return computerScore++; 
+    else if (humanChoice === "COFFEE MACHINE" && computerChoice === "BOSS") {
+      para.textContent = "You won!";
+      para1.textContent = "Coffee Machine malfunctions for Boss";
+      humanScore++;
     }
 
-    else
-      console.log("Please enter a valid choice");
+    else if (computerChoice === "BOSS" && humanChoice === "INTERN") {
+      para.textContent = "You lost!";
+      para1.textContent = "Boss reprimands Intern";
+      computerScore++;
+    }
+    
+    else if (computerChoice === "INTERN" && humanChoice === "COFFEE MACHINE") {
+      para.textContent = "You lost!";
+      para1.textContent = "Intern breaks Coffee Machine";
+      computerScore++;
+    }
+
+    else if (computerChoice === "COFFEE MACHINE" && humanChoice === "BOSS") {
+      para.textContent = "You lost!";
+      para1.textContent = "Coffee Machine malfunctions for Boss";
+      computerScore++; 
+    }
+  
+  
+  const yourScore = document.querySelector(".yourScore");
+  yourScore.textContent = humanScore;
+
+  const cpuScore = document.querySelector(".cpuScore");
+  cpuScore.textContent = computerScore;
+
+  if(humanScore === 5) {
+    para.textContent = "YAY!";
+    para1.textContent = "You Won";
+    buttons.removeEventListener('click', gameHandler);
   }
   
-  let humanScore = 0, 
-  computerScore = 0;
-
-  function playGame() {
-
-  for(let i = 1; i <= 5; i++) {
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    console.log("Your score is " + humanScore + "," + " Computer's score is " + computerScore);
+  else if(computerScore === 5) {
+    para.textContent = "Sorry";
+    para1.textContent = "You Lost";
+    buttons.removeEventListener('click', gameHandler);
   }
   
-  if(humanScore > computerScore) {
-    console.log("You win");
-  }
+};
 
-  else {
-    console.log("You lost");
+  function gameHandler(e) {
+    let humanChoice = getHumanChoice(e);
+
+    if(humanChoice && start) {
+      let computerChoice = getComputerChoice();
+      playRound(humanChoice, computerChoice);
+    }
+  };
+
+  const startBtn = document.querySelector(".start");
+  let start = false;
+
+  function startIsClicked(e) {
+    return e.isTrusted;;
   }
+ 
+  startBtn.addEventListener('click', (e) => {
+    if (startIsClicked(e)) {
+       start = true;
+    }
+  });
   
- }
+  
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const reloadButton = document.querySelector('.restart');
+    
+    reloadButton.addEventListener('click', () => {
+        location.reload();
+    });
+});
 
- playGame();
+
+
+  buttons.addEventListener('click', gameHandler);
+  
+ 
+  
+  
+
+  
+
+  
+
+  
